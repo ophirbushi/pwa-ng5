4f485067bc1d64f1c51fa5a7fcbef4518f7ee56f
ophir <ophirbushi@gmail.com>
2017-11-27 22:00:07 +0200

    .

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@ const
   gulp = require('gulp'),
   swPrecache = require('sw-precache');
 
-const srcPath = './src';
+const outPath = './dist';
 
 gulp.task('service-worker', () => {
-  const swPath = `${srcPath}/sw.js`;
+  const swPath = `${outPath}/sw.js`;
 
   swPrecache.write(swPath, {
     staticFileGlobs: ['dist/**/*.{js,html,css,png,jpg,gif}'],
-    stripPrefix: 'dist',
+    stripPrefix: 'dist'
     // runtimeCaching: [{
     //   urlPattern: /^https:\/\/example\.com\/api/,
     //   handler: 'networkFirst'
